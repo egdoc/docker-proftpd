0d6677985598e13cb36f0813a40e4376370568f9
Egidio Docile <egdoc@users.noreply.github.com>
2024-03-26 17:27:44 +0100

    Fixed variable name

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -o errexit
 set -o nounset
 
 # We set ownership here in case the path is used as a volume
-chown "${FTP_USER}":"${FTP_USER}" "/home/${USER}"
+chown "${FTP_USER}":"${FTP_USER}" "/home/${FTP_USER}"
 
 echo "${FTP_USER}:${FTP_PASS}" | /usr/sbin/chpasswd
 
